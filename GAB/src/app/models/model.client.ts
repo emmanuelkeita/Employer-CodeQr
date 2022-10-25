a091a28e84c4ca22e1ed1e44ff229ac99c92ad41
emmanuel <emmakeita04@gmail.com>
2022-10-25 16:21:18 +0000

    paulRousseu

---
+++ GAB/src/app/models/model.client.ts
@@ -0,0 +1,29 @@
+export class Client {
+    prenom: string;
+    nom:string;
+    tel:number;
+    imageUrl:string;
+    imageEchantillon:string;
+    dateArrivee:Date;
+    datelivraison:Date;
+    prix:number;
+    avance:number;
+    reste :number;
+    modele:string;
+    Rdv:number;
+    e:number;
+    m:number;
+    p:number;
+    l:number;
+    c :number;
+    cu:number;
+    f:number;
+    ln :number;
+    d :number;
+    tb :number;
+    bis :number;
+    poi :number;
+    cou :number;
+    bas :number;
+    pes :number;
+}
